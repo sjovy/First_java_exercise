dd20dcb4d31f340c7ecc3597f3f4a3c5797cb11e
sjovy <thomas.sjovy@gmail.com>
2024-02-28 13:43:20 +0100

    test commit

---
+++ src/main/java/se/lexicon/App.java
@@ -0,0 +1,17 @@
+package se.lexicon;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");   //How to comment
+        System.out.println("Hello Java");
+    }
+}
+
+/*
+    Multi-
+    line
+    comment
+ */
